88e4a466d1552ae39a7419cd5e790194645035b7
Sardar219 <sardarhossaini2022@gmail.com>
2025-05-07 11:00:13 +0430

    added edit note page

---
+++ lib/edit.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+import 'note.dart';
+
+class EditNoteScreen extends StatefulWidget {
+  const EditNoteScreen({
+    super.key,
+    required this.noteIndex,
+    required this.lineIndex,
+  });
+  final int noteIndex;
+  final int lineIndex;
+  @override
+  State<EditNoteScreen> createState() => _EditNoteScreenState();
+}
+
+class _EditNoteScreenState extends State<EditNoteScreen> {
+  TextEditingController titleEditingController = TextEditingController();
+  TextEditingController descriptionEditingController = TextEditingController();
+  bool isNewNote = false;
+  int index = -1;
+  int lineIndex = -1;
+  @override
+  void initState() {
+    super.initState();
+    index = widget.noteIndex;
+    lineIndex = widget.lineIndex;
+    isNewNote = index == -1;
+    if (isNewNote) {
+      titleEditingController.text = NEW_NOTE_TITLE;
+      descriptionEditingController.text = NEW_NOTE_BODY;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          CREATE_NEW_NOTE,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: YELLOW,
+        foregroundColor: BLACK,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {},
+            tooltip: SAVE,
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+            tooltip: DELETE,
+          ),
+        ],
+      ),
+    );
+  }
+}
